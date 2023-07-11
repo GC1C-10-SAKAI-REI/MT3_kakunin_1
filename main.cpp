adce01563cda5dec2d4178e114e634a59d4406f2
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-07-11 14:15:37 +0900

    コードの初期化

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 
 const char kWindowTitle[] = "GC2C_07_サカイレイ";
 
-
-
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -17,11 +15,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-
-	MyMatrix* myMatrix = new MyMatrix();
-
-	//カメラの位置
-	Vec3 cameraPos{ 0.0f,1.9f,-6.49f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0)
@@ -35,16 +28,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		/// ↓更新処理ここから
 		
-		//1.
-		Matrix4x4 cameraMatrix = myMatrix->MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.26f,0.0f,0.0f }, cameraPos);
-		//2.ビュー座標系に変換
-		Matrix4x4 viewMatrix = myMatrix->Inverse(cameraMatrix);
-		//3.ビュー座標系から同時クリップ座標系に変換
-		Matrix4x4 projectionMatrix = myMatrix->MakePerspectiveFovMatrix(0.45f, float(WINDOW_WIDTH) / float(WINDOW_HEIGHT), 0.1f, 100.0f);
-		//4.
-		Matrix4x4 viewProjectionMatrix = myMatrix->Multiply(viewMatrix, projectionMatrix);
-		//5.正規化デバイス座標系からスクリーン座標系に変換
-		Matrix4x4 viewportMatrix = myMatrix->MakeViewportMatrix(0, 0, float(WINDOW_WIDTH), float(WINDOW_HEIGHT), 0.0f, 1.0f);
+		
 		
 		/// ↑更新処理ここまで
 		
@@ -52,7 +36,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		
 		/// ↓描画処理ここから
 		
-		myMatrix->DrawGrid(viewProjectionMatrix, viewportMatrix);
+		
 		
 		/// ↑描画処理ここまで		
 
